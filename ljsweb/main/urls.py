32cb959dd8def01291bafb66a6516be49608aafe
Djangoubu <affanhmalik@gmail.com>
2014-09-25 13:44:16 -0700

    Fully functional and test auth system

--- ljsweb/main/urls.py
+++ ljsweb/main/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from main import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,4 +9,9 @@ urlpatterns = patterns('',
 
 
     url(r'^$', TemplateView.as_view(template_name="main/index.html")),
+    url(r'^home/$', views.loggedin, name='loggedin'),
+    url(r'^signup/$', TemplateView.as_view(template_name="main/signup.html")),
+    url(r'^login/$', views.auth_and_login, name='login'),
+    url(r'^logout/$', views.logout_user, name='logout'),
+    url(r'^register/$', views.sign_up_in, name='register'),
 )
--- ljsweb/main/views.py
+++ ljsweb/main/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+import json
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+# The main authentication function for a registered user, the success and fail redirects can be changed
+def auth_and_login(request, onsuccess='/main/home/', onfail='/main/signup/'):
+	user = authenticate(username=request.POST['username'], password=request.POST['password'])
+	if user is not None:
+		login(request, user)
+		return redirect(onsuccess)
+	else:
+		return redirect(onfail)
+
+def sign_up_in(request):
+	post = request.POST
+	if not user_exists(post['username']):
+		user = create_user(username=post['username'], password=post['password'])
+		
+		return auth_and_login(request)
+	else:
+		return redirect("/main/")
+
+@login_required
+def loggedin(request):
+	return render_to_response('main/test.html',{'user':request.user})
+
+
+# Create a new user with the request credentials
+def create_user(username,password):
+	user = User(username=username)
+	user.set_password(password)
+	user.save()
+	return user
+
+# Check to see if username exists or not
+def user_exists(username):
+	user_count = User.objects.filter(username=username).count()
+	if user_count == 0:
+		return False
+	return True
+
+def logout_user(request):
+	logout(request)
+	return redirect("/main/")
